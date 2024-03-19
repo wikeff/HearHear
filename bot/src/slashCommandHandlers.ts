a4b7a5eca79631730c348a2404cf7f35467e3987
Xipu Li <lixipu98@gmail.com>
2024-03-18 18:28:51 -0700

    chore: formatter setup; format files

--- bot/src/slashCommandHandlers.ts
+++ bot/src/slashCommandHandlers.ts
@@ -3,14 +3,15 @@
  */
 
 import {
-	// entersState,
-	joinVoiceChannel,
-	VoiceConnection,
-	// VoiceConnectionStatus,
-} from '@discordjs/voice';
-import { Client, CommandInteraction, GuildMember } from 'discord.js';
-import { createListeningStream } from './utils/audio';
-import { processRecordings, uploadRecordingsToS3 } from './utils/audioProcessor';
+  // entersState,
+  joinVoiceChannel,
+  VoiceConnection,
+  // VoiceConnectionStatus,
+} from "@discordjs/voice";
+import { Client, CommandInteraction, GuildMember } from "discord.js";
+import { createListeningStream } from "./utils/audio";
+import { processRecordings } from "./utils/audioProcessor";
+import { uploadRecordingsToS3 } from "./utils/s3";
 
 /**
  * Handles the '/start' command.
@@ -26,93 +27,125 @@ import { processRecordings, uploadRecordingsToS3 } from './utils/audioProcessor'
  * @param connection
  * @returns
  */
-async function start(interaction: CommandInteraction, client: Client, connection?: VoiceConnection) {
-	await interaction.deferReply();
-	if (!connection) {
-		// Check if the user provoking the command is a real human and  is in a voice channel
-		if (interaction.member instanceof GuildMember && interaction.member.voice.channel) {
-			const channel = interaction.member.voice.channel;
-			connection = joinVoiceChannel({
-				channelId: channel.id,
-				guildId: channel.guild.id,
-				selfDeaf: false,
-				selfMute: true,
-				adapterCreator: channel.guild.voiceAdapterCreator,
-			});
-		} else {
-			await interaction.editReply('Join a voice channel and then try that again!');
-			return;
-		}
-	}
+async function start(
+  interaction: CommandInteraction,
+  client: Client,
+  connection?: VoiceConnection
+) {
+  await interaction.deferReply();
+  if (!connection) {
+    // Check if the user provoking the command is a real human and  is in a voice channel
+    if (
+      interaction.member instanceof GuildMember &&
+      interaction.member.voice.channel
+    ) {
+      const { channel } = interaction.member.voice;
+      connection = joinVoiceChannel({
+        channelId: channel.id,
+        guildId: channel.guild.id,
+        selfDeaf: false,
+        selfMute: true,
+        adapterCreator: channel.guild.voiceAdapterCreator,
+      });
+    } else {
+      await interaction.editReply(
+        "Join a voice channel and then try that again!"
+      );
+      return;
+    }
+  }
 
-	// At this point, the bot is in a voice channel and is actively
-	// listening to the voice channel for any audio input.
-	try {
-		// await entersState(connection, VoiceConnectionStatus.Ready, 20e3);
-		const receiver = connection.receiver;
+  // At this point, the bot is in a voice channel and is actively
+  // listening to the voice channel for any audio input.
+  try {
+    // await entersState(connection, VoiceConnectionStatus.Ready, 20e3);
+    const { receiver } = connection;
 
-		receiver.speaking.on('start', (userId) => {
-			console.log('start', userId);
-			if (interaction.member instanceof GuildMember && interaction.member.voice.channel) {
-				const channel = interaction.member.voice.channel;
-				createListeningStream(receiver, userId, channel.guild.id, client.users.cache.get(userId));
-			}
-		});
-	} catch (error) {
-		console.warn(error);
-		await interaction.editReply('Failed to join voice channel within 20 seconds, please try again later!');
-	}
+    receiver.speaking.on("start", (userId) => {
+      console.log("start", userId);
+      if (
+        interaction.member instanceof GuildMember &&
+        interaction.member.voice.channel
+      ) {
+        const { channel } = interaction.member.voice;
+        createListeningStream(
+          receiver,
+          userId,
+          channel.guild.id,
+          client.users.cache.get(userId)
+        );
+      }
+    });
+  } catch (error) {
+    console.warn(error);
+    await interaction.editReply(
+      "Failed to join voice channel within 20 seconds, please try again later!"
+    );
+  }
 
-	await interaction.editReply('Ready!');
+  await interaction.editReply("Ready!");
 }
 
-async function end(interaction: CommandInteraction, _client: Client, connection?: VoiceConnection) {
-	if (connection) {
-		await interaction.deferReply({ ephemeral: true });
-		connection.receiver.subscriptions.forEach((subscription) => {
-			subscription.emit('end');
-		});
-		connection.destroy();
+async function end(
+  interaction: CommandInteraction,
+  _client: Client,
+  connection?: VoiceConnection
+) {
+  if (connection) {
+    await interaction.deferReply({ ephemeral: true });
+    connection.receiver.subscriptions.forEach((subscription) => {
+      subscription.emit("end");
+    });
+    connection.destroy();
 
-		await interaction.editReply({
-			content: 'Stopped recording audio file',
-		});
-		await interaction.editReply({
-			content: 'Process recordings...this may take a while',
-		});
+    await interaction.editReply({
+      content: "Stopped recording audio file",
+    });
+    await interaction.editReply({
+      content: "Process recordings...this may take a while",
+    });
 
-		if (interaction.member instanceof GuildMember && interaction.member.voice.channel && interaction.guildId) {
-			const guildId = interaction.guildId;
-			const channelId = interaction.member.voice.channel.id;
-			processRecordings(guildId)
-				.then(async () => {
-					await uploadRecordingsToS3(guildId, channelId);
-					await interaction.editReply({
-						// TODO: change the link
-						content: `Completed! You can now head to https://hearhear.vercel.app/dashboard/guilds/${guildId}/ to view your meetings!`,
-					});
-				})
-				.catch(async (err) => {
-					await interaction.editReply({
-						content: `Failed to upload the files to S3! ${err}`,
-					});
-				});
-		} else {
-			await interaction.editReply({
-				content: 'Failed to retrieve information needed to upload the files to S3. Need guildId and channelId!',
-			});
-		}
-	} else {
-		await interaction.reply({
-			ephemeral: true,
-			content: 'Not recording in this server!',
-		});
-	}
+    if (
+      interaction.member instanceof GuildMember &&
+      interaction.member.voice.channel &&
+      interaction.guildId
+    ) {
+      const { guildId } = interaction;
+      const channelId = interaction.member.voice.channel.id;
+      processRecordings(guildId)
+        .then(async () => {
+          await uploadRecordingsToS3(guildId, channelId);
+          await interaction.editReply({
+            // TODO: change the link
+            content: `Completed! You can now head to https://hearhear.vercel.app/dashboard/guilds/${guildId}/ to view your meetings!`,
+          });
+        })
+        .catch(async (err) => {
+          await interaction.editReply({
+            content: `Failed to upload the files to S3! ${err}`,
+          });
+        });
+    } else {
+      await interaction.editReply({
+        content:
+          "Failed to retrieve information needed to upload the files to S3. Need guildId and channelId!",
+      });
+    }
+  } else {
+    await interaction.reply({
+      ephemeral: true,
+      content: "Not recording in this server!",
+    });
+  }
 }
 
 export const interactionHandlers = new Map<
-	string,
-	(interaction: CommandInteraction, client: Client, connection?: VoiceConnection) => Promise<void>
+  string,
+  (
+    interaction: CommandInteraction,
+    client: Client,
+    connection?: VoiceConnection
+  ) => Promise<void>
 >();
-interactionHandlers.set('start', start);
-interactionHandlers.set('end', end);
+interactionHandlers.set("start", start);
+interactionHandlers.set("end", end);
--- bot/src/utils/s3.ts
+++ bot/src/utils/s3.ts
@@ -1,13 +1,16 @@
-import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
+import * as fs from "fs";
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import { config } from "dotenv";
+import { deleteFileSync } from "./files.js";
+import { Strings } from "../tokens/constants.js";
 
-import { config } from 'dotenv';
 config();
 
-const S3_BUCKET_REGION = process.env.S3_BUCKET_REGION;
-const S3_BUCKET_NAME = process.env.S3_BUCKET_NAME;
+const { S3_BUCKET_REGION } = process.env;
+const { S3_BUCKET_NAME } = process.env;
 
 const client = new S3Client({
-	region: S3_BUCKET_REGION!,
+  region: S3_BUCKET_REGION!,
 });
 
 /**
@@ -23,28 +26,108 @@ const client = new S3Client({
  * @returns
  */
 async function uploadMeetingFileToS3(
-	data: Buffer,
-	fileName: string,
-	guildId: string,
-	channelId: string,
-	timestamp: string,
-	contentType: string = 'audio/ogg',
+  data: Buffer,
+  fileName: string,
+  guildId: string,
+  channelId: string,
+  timestamp: string,
+  contentType = "audio/ogg"
 ) {
-	const params = {
-		Bucket: S3_BUCKET_NAME,
-		Key: `${guildId}/${channelId}/${timestamp}/${fileName}`,
-		Body: data,
-		ContentType: contentType,
-	};
+  const params = {
+    Bucket: S3_BUCKET_NAME,
+    Key: `${guildId}/${channelId}/${timestamp}/${fileName}`,
+    Body: data,
+    ContentType: contentType,
+  };
 
-	try {
-		const results = await client.send(new PutObjectCommand(params));
-		console.log('Successfully created ' + params.Key + ' and uploaded it to ' + params.Bucket + '/' + params.Key);
-		return results; // For unit tests.
-	} catch (err) {
-		console.log('Error', err);
-		return err; // For unit tests.
-	}
+  try {
+    const results = await client.send(new PutObjectCommand(params));
+    console.log(
+      `Successfully created ${params.Key} and uploaded it to ${params.Bucket}/${params.Key}`
+    );
+    return results; // For unit tests.
+  } catch (err) {
+    console.log("Error", err);
+    return err; // For unit tests.
+  }
 }
 
-export { uploadMeetingFileToS3 };
+async function uploadRecordingsToS3(guildId: string, channelId: string) {
+  const files = fs
+    .readdirSync(`${Strings.RECORDING_FILES_PATH}/${guildId}`)
+    .filter((file) => file.endsWith(".ogg"));
+
+  if (files.length === 0) {
+    console.log("No files to upload");
+    return;
+  }
+
+  // sort files by date
+  files.sort((a, b) => {
+    const aDate = new Date(a.split("-")[0]!);
+    const bDate = new Date(b.split("-")[0]!);
+    return aDate.getTime() - bDate.getTime();
+  });
+
+  const earliestDate = files[0]!.split("-")[0]!;
+
+  await Promise.all(
+    files.map(async (file) => {
+      fs.readFile(
+        `${Strings.RECORDING_FILES_PATH}/${guildId}/${file}`,
+        async (err, data) => {
+          if (err) {
+            console.log(err);
+            return;
+          }
+          await uploadMeetingFileToS3(
+            data,
+            file,
+            guildId,
+            channelId,
+            earliestDate
+          )
+            .then(() => {
+              deleteFileSync(
+                `${Strings.RECORDING_FILES_PATH}/${guildId}/${file}`
+              );
+            })
+            .catch((err: any) => {
+              console.log(err);
+            });
+        }
+      );
+
+      fs.readFile(
+        `${Strings.RECORDING_FILES_PATH}/${guildId}/${file.slice(0, -4)}.txt`,
+        async (err, data) => {
+          if (err) {
+            console.log(err);
+            return;
+          }
+          await uploadMeetingFileToS3(
+            data,
+            `${file.slice(0, -4)}.txt`,
+            guildId,
+            channelId,
+            earliestDate,
+            "text/plain"
+          )
+            .then(() => {
+              deleteFileSync(
+                `${Strings.RECORDING_FILES_PATH}/${guildId}/${file.slice(
+                  0,
+                  -4
+                )}.txt`
+              );
+            })
+            .catch((err: any) => {
+              console.log(err);
+            });
+        }
+      );
+    })
+  );
+}
+
+export { uploadMeetingFileToS3, uploadRecordingsToS3 };
